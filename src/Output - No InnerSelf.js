5d6897e98e7cd1b35853ed5b83d1d818d107a631
Fieldnote-AID <sethp61@gmail.com>
2026-04-01 15:15:36 -0700

    Update Output - No InnerSelf.js

--- src/Output - No InnerSelf.js
+++ src/Output - No InnerSelf.js
@@ -1,7 +1,5 @@
-// Your "Output" tab should look like this
 const modifier = (text) => {
-  try { text = QuestDirectorHooks.output(text); } catch (e) {}
-  // Any other output modifier scripts can go here
+  text = QuestDirectorHooks.output(text);
   return { text };
 };
 modifier(text);
