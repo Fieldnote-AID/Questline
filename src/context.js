0c317ab8fc1c07b6f9ef7b07e461cec4d54d8e82
Fieldnote-AID <sethp61@gmail.com>
2026-04-01 15:14:29 -0700

    Update context.js

--- src/context.js
+++ src/context.js
@@ -1,7 +1,5 @@
-// Your "Context" tab should look like this
 InnerSelf("context");
 const modifier = (text) => {
-  // Any other context modifier scripts can go here
   return { text, stop };
 };
 modifier(text);
